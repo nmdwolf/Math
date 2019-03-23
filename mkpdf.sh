4935e0a57da9dc4986e5a7c9b634975d6c3a9ff3
Nicolas Dewolf <nicolas.dewolf@ugent.be>
2019-03-23 15:41:17 +0100

    major update

--- mkpdf.sh
+++ mkpdf.sh
@@ -1,6 +1,6 @@
-latexmk -synctex=1 -pdf main.tex
+pdflatex main.tex
 makeindex main.nlo -s nomencl.ist -o main.nls
+texindy -C utf8 -o main.ind main.idx
 bibtex main
-latexmk -synctex=1 -pdf main.tex
 latexmk -synctex=1 -pdf main.tex
 xdg-open main.pdf
